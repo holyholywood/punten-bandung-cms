2ca39c0effd475bc1efe3a9bfc20d690e20bacfd
Sihkdragon <ditolord@gmail.com>
2023-03-15 23:46:52 +0700

    add image processing and api prepare for post news

--- src/pages/tabloids/news/write.jsx
+++ src/pages/tabloids/news/write.jsx
@@ -1,117 +1,142 @@
-import { Divider, Image, Input, Typography } from 'antd'
-import { FileImageOutlined } from '@ant-design/icons'
-import React, { useReducer, useState } from 'react'
-import dynamic from 'next/dynamic'
-import 'react-quill/dist/quill.snow.css'
-import TagPill from '@/components/atoms/TagPill'
-const ReactQuill = dynamic(import('react-quill'), { ssr: false })
-const { Text, Title } = Typography
+import { Button, Divider, Image, Input, Typography } from "antd";
+import { FileImageOutlined, SendOutlined } from "@ant-design/icons";
+import React, { useReducer, useState } from "react";
+import dynamic from "next/dynamic";
+import "react-quill/dist/quill.snow.css";
+import TagPill from "@/components/atoms/TagPill";
+const ReactQuill = dynamic(import("react-quill"), { ssr: false });
+const { Text, Title } = Typography;
 function reducer(state, action) {
-  if (action.type === 'change_title') {
+  if (action.type === "change_title") {
     return {
       ...state,
-      title: action.newTitle
-    }
+      title: action.newTitle,
+    };
   }
-  if (action.type === 'change_body') {
+  if (action.type === "change_body") {
     return {
       ...state,
-      body: action.newBody
-    }
+      body: action.newBody,
+    };
   }
-  return new Error('no Action recognized for reducer ' + action.type)
+  return new Error("no Action recognized for reducer " + action.type);
 }
 const WriteNewsPage = () => {
-  const [state, dispatch] = useReducer(reducer, { title: '', body: '' })
-  const [tags, setTags] = useState([])
-  const [tagInput, setTagInput] = useState('')
+  const [state, dispatch] = useReducer(reducer, { title: "", body: "" });
+  const [imagePreview, setImgPreview] = useState("");
+  const [image, setImage] = useState(null);
+  const [tags, setTags] = useState([]);
+  const [tagInput, setTagInput] = useState("");
   const tagSetter = (e) => {
-    setTagInput(e.target.value)
+    setTagInput(e.target.value);
     if (tagInput.length > 1 && tags.length < 5) {
       if (
-        tagInput.charAt(tagInput.length - 1) === ',' ||
-        tagInput.charAt(tagInput.length - 1) === ' '
+        tagInput.charAt(tagInput.length - 1) === "," ||
+        tagInput.charAt(tagInput.length - 1) === " "
       ) {
-        let word = tagInput.substring(0, tagInput.length - 1)
-        setTagInput('')
+        let word = tagInput.substring(0, tagInput.length - 1);
+        setTagInput("");
 
-        setTags([...tags, word])
+        setTags([...tags, word]);
       }
     } else if (tags.length >= 5) {
-      setTagInput('')
+      setTagInput("");
     }
-  }
+  };
+  const imageUploadHandler = (e) => {
+    setImage(e.target.files[0]);
+    const preview = URL.createObjectURL(e.target.files[0]);
+    setImgPreview(preview);
+  };
+
+  const handleSubmit = () => {
+    const data = {
+      title: state.title,
+      body: state.body,
+      image: image,
+      tags,
+    };
+    console.log(data);
+  };
   return (
-    <div className='min-h-screen'>
+    <div className="min-h-screen">
       <Title>Tulis Berita</Title>
       <Divider />
-      <div className='flex flex-col gap-4 min-h-screen'>
-        <div className='flex gap-5'>
-          <div className='w-1/2'>
+      <div className="flex flex-col gap-4 min-h-screen">
+        <div className="flex gap-5">
+          <div className="w-1/2">
             <label
-              htmlFor='image-cover'
-              className='flex
-             gap-4 text-xl items-center hover:cursor-pointer'
+              htmlFor="image-cover"
+              className="flex
+             gap-4 text-xl items-center hover:cursor-pointer"
             >
-              <figure className='w-full aspect-[16/9] bg-gray-100 flex items-center justify-center'>
-                {/* <Image src=""/> */}
-                <div className='text-gray-500'>
-                  <FileImageOutlined />
-                  <Text>Tambah cover berita</Text>
-                </div>
+              <figure className="w-full overflow-hidden aspect-[16/9] bg-gray-100 flex items-center justify-center">
+                {imagePreview ? (
+                  <img src={imagePreview} className="object-fit w-full" />
+                ) : (
+                  <div className="text-gray-500">
+                    <FileImageOutlined />
+                    <Text>Tambah cover berita</Text>
+                  </div>
+                )}
               </figure>
             </label>
             <Input
-              type='file'
-              id='image-cover'
-              accept='image/png, image/gif, image/jpeg'
-              className='hidden'
+              type="file"
+              id="image-cover"
+              accept="image/png, image/gif, image/jpeg"
+              className="hidden"
+              onChange={imageUploadHandler}
             />
           </div>
-          <div className='w-1/2 flex flex-col gap-5'>
-            <label htmlFor='title' className=''>
+          <div className="w-1/2 flex flex-col gap-5">
+            <div className="flex justify-end items-center gap-2">
+              <Button type="primary" size="large" onClick={handleSubmit}>
+                Post
+                <SendOutlined />
+              </Button>
+            </div>
+            <label htmlFor="title" className="">
               Judul
             </label>
             <Input
-              type='text'
-              id='title'
-              size='large'
-              className=''
-              onChange={(e) => dispatch({ type: 'change_title', newTitle: e.target.value })}
+              type="text"
+              id="title"
+              size="large"
+              className=""
+              value={state.title}
+              onChange={(e) => dispatch({ type: "change_title", newTitle: e.target.value })}
             />
-            <label htmlFor='tag' className='mt-5'>
+            <label htmlFor="tag" className="mt-5">
               Tag
             </label>
             <Input
-              type='text'
-              id='tag'
-              size='large'
+              type="text"
+              id="tag"
+              size="large"
               onChange={tagSetter}
               value={tagInput}
-              className=''
+              className=""
             />
-            <div className='flex items-center gap-1'>
+            <div className="flex items-center gap-1">
               {tags.map((el, i) => (
                 <TagPill text={el} key={i} />
               ))}
             </div>
           </div>
         </div>
-        <label htmlFor='body' className='mt-5'>
-          Body
-        </label>
         <ReactQuill
-          theme='snow'
-          className='h-full'
-          id='body'
-          onChange={(e) => dispatch({ type: 'change_body', newBody: e })}
+          theme="snow"
+          className="h-full [&>.ql-container]:min-h-[50vh]"
+          id="body"
+          onChange={(e) => dispatch({ type: "change_body", newBody: e })}
           style={{
-            minHeight: '10rem'
+            minHeight: "10rem",
           }}
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default WriteNewsPage
+export default WriteNewsPage;
--- src/utils/API/Tabloid.js
+++ src/utils/API/Tabloid.js
@@ -1,58 +1,64 @@
-import Fetch from '.'
+import Fetch from ".";
 
 class TabloidModel {
   constructor() {
-    this.BASE_SLUG = NEXT_PUBLIC_BASE_TABLOID
+    this.BASE_SLUG = NEXT_PUBLIC_BASE_TABLOID;
   }
-  async get(slug = '') {
+  async get(slug = "") {
     try {
-      return await Fetch.get(this.BASE_SLUG + slug)
+      return await Fetch.get(this.BASE_SLUG + slug);
     } catch (error) {
-      return error
+      return error;
     }
   }
   async show(params) {
-    if (!params) throw new Error('No ID Preserved')
+    if (!params) throw new Error("No ID Preserved");
 
     try {
-      return await Fetch.get(this.BASE_SLUG + '/' + params)
+      return await Fetch.get(this.BASE_SLUG + "/" + params);
     } catch (error) {
-      return error
+      return error;
     }
   }
 
   async store(data) {
-    if (!data) throw new Error('No Data Preserved')
+    if (!data) throw new Error("No Data Preserved");
 
+    formData.append("title", data.title);
+    formData.append("image", data.image);
+    formData.append("body", data.body);
+    for (let i = 1; i <= data.tags.length - 1; i++) {
+      formData.append(`tag${i}`, data.tags[i - 1]);
+    }
     try {
-      return await Fetch.get(this.BASE_SLUG, data)
+      return await Fetch.post(this.BASE_SLUG, data);
     } catch (error) {
-      return error
+      return error;
     }
   }
 
   async update(id, data) {
-    if (!id) throw new Error('No ID Preserved')
-    if (!data) throw new Error('No Data Preserved')
+    if (!id) throw new Error("No ID Preserved");
+    if (!data) throw new Error("No Data Preserved");
 
     try {
-      return await Fetch.get(this.BASE_SLUG, data)
+      return await Fetch.get(this.BASE_SLUG, data);
     } catch (error) {
-      return error
+      return error;
     }
   }
 
   async delete(id) {
-    if (!id) throw new Error('No ID Preserved')
+    if (!id) throw new Error("No ID Preserved");
 
     try {
-      return await Fetch.get(this.BASE_SLUG)
+      return await Fetch.get(this.BASE_SLUG);
     } catch (error) {
-      return error
+      return error;
     }
   }
 }
 
-const Tabloid = new TabloidModel()
+const Tabloid = new TabloidModel();
 
-export default Tabloid
+export default Tabloid;
